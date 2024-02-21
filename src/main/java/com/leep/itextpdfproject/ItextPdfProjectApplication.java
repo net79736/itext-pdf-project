da3c0c6484caeb876a2656fecc7d696b0f6c3977
leejongwook2 <34103253+leejongwook2@users.noreply.github.com>
2024-02-21 22:54:10 +0900

    Initial commit

---
+++ src/main/java/com/leep/itextpdfproject/ItextPdfProjectApplication.java
@@ -0,0 +1,13 @@
+package com.leep.itextpdfproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItextPdfProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItextPdfProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leep/itextpdfproject/ItextPdfProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leep.itextpdfproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItextPdfProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
